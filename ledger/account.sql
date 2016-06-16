8a65d9942071d8b22cd3031f6a7b805cc5d79d5f
dyani17 <lomilee1007@gmail.com>
2016-06-16 22:18:45 +0900

    Delete account.sql

--- ledger/account.sql
+++
@@ -1,9 +0,0 @@
-create table account(
-	fullname varchar(30) not null,
-	username varchar(20) not null,
-	password varchar(20) not null,
-	email varchar(320) not null,
-	question text not null,
-	answer varchar(30) not null,
-	primary key (username)
-);
